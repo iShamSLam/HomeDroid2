aa3fb1b477636d1a7f8bc41bc4db6993752ad337
NikitaKhokhlov11-708 <nkhokhlov11708@gmail.com>
2019-03-06 22:40:37 +0300

    Tommy Cache

--- app/src/main/java/com/example/homedroid2/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/homedroid2/presenter/MainPresenter.kt
@@ -2,25 +2,29 @@
 
 package com.example.homedroid2.presenter
 
+import android.icu.util.ULocale
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.example.homedroid2.models.GoodreadsApiService
 import com.example.homedroid2.views.MainView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import okhttp3.Cache
+import okhttp3.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.OkHttpClient
 import java.io.File
+import java.io.IOException
+import okhttp3.OkHttpClient
+
+
 
 
 @InjectViewState
 class MainPresenter() : MvpPresenter<MainView>() {
     private val BASE_URL = "https://www.goodreads.com/"
-    private lateinit var okHttpClient: OkHttpClient
+    private lateinit var okHttpClient: OkHttpClient.Builder
 
     public override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -34,7 +38,7 @@ class MainPresenter() : MvpPresenter<MainView>() {
     fun loadXML(query: String = "") {
         val requestInterface = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(okHttpClient)
+            .client(okHttpClient.build())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(SimpleXmlConverterFactory.create())
             .build().create(GoodreadsApiService::class.java)
@@ -47,12 +51,10 @@ class MainPresenter() : MvpPresenter<MainView>() {
 //        )
     }
 
-    fun createCache(file: File) {
-        val cacheSize : Long = 10 * 1024 * 1024 // 10 MB
+    fun createCache(file: File, REWRITE_CACHE_CONTROL_INTERCEPTOR : Interceptor) {
+        val cacheSize: Long = 10 * 1024 * 1024 // 10 MB
         val cache = Cache(file, cacheSize)
-
-        okHttpClient = OkHttpClient.Builder()
-            .cache(cache)
-            .build()
+        okHttpClient = OkHttpClient.Builder().cache(cache)
+        okHttpClient.networkInterceptors().add(REWRITE_CACHE_CONTROL_INTERCEPTOR)
     }
 }
--- app/src/main/java/com/example/homedroid2/views/MainActivity.kt
+++ app/src/main/java/com/example/homedroid2/views/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.homedroid2.views
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -17,7 +20,10 @@ import io.apptitude.premiumparking.utils.functions.observableFromSearchView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.Interceptor
+import okhttp3.Response
 import java.io.File
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 
@@ -37,7 +43,25 @@ class MainActivity : MvpAppCompatActivity(), MainView, DataAdapter.BooksViewHold
         setContentView(R.layout.activity_main)
         setupViews()
 
-        mMainPresenter.createCache(cacheDir)
+        val REWRITE_CACHE_CONTROL_INTERCEPTOR = object : Interceptor {
+            @Throws(IOException::class)
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val originalResponse = chain.proceed(chain.request())
+                if (isNetworkAvailable()) {
+                    val maxAge = 60 // read from cache for 1 minute
+                    return originalResponse.newBuilder()
+                        .header("Cache-Control", "public, max-age=" + maxAge)
+                        .build()
+                } else {
+                    val maxStale = 60 * 60 * 24 * 28 // tolerate 4-weeks stale
+                    return originalResponse.newBuilder()
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                        .build()
+                }
+            }
+        }
+
+        mMainPresenter.createCache(cacheDir, REWRITE_CACHE_CONTROL_INTERCEPTOR)
 
         observableFromSearchView(sv_main)
             .debounce(1, TimeUnit.SECONDS)
@@ -47,6 +71,15 @@ class MainActivity : MvpAppCompatActivity(), MainView, DataAdapter.BooksViewHold
                     mMainPresenter.loadXML(it)
             }, onError = {})
     }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+    }
+
 
     override fun getBooks(query: String?) {
         mMainPresenter.loadXML()
